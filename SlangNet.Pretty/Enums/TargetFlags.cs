32a3cd6894c9f99c5f83c863e9953c16d4dd98e6
Alexsixcent <alexsixcent@gmail.com>
2025-01-20 12:45:35 +0000

    Fix enum wrong backing type

--- SlangNet.Pretty/Enums/TargetFlags.cs
+++ SlangNet.Pretty/Enums/TargetFlags.cs
@@ -3,7 +3,7 @@
 namespace SlangNet;
 
 [Flags]
-public enum TargetFlags
+public enum TargetFlags : uint
 {
     ParameterBlocksUseRegisterSpaces = SLANG_TARGET_FLAG_PARAMETER_BLOCKS_USE_REGISTER_SPACES,
     GenerateWholeProgram = SLANG_TARGET_FLAG_GENERATE_WHOLE_PROGRAM,
