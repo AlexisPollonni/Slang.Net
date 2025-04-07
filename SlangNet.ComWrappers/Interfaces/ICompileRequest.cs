a4928dbaaca6c39d53f6f4d5ac2cac2d87747176
AlexisPollonni <alexis.pollonni@gmail.com>
2025-04-07 14:21:37 +0200

    Added ICompileRequest placeholder

---
+++ SlangNet.ComWrappers/Interfaces/ICompileRequest.cs
@@ -0,0 +1,11 @@
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace SlangNet.ComWrappers;
+
+[GeneratedComInterface(StringMarshalling = StringMarshalling.Utf8, Options = ComInterfaceOptions.ComObjectWrapper)]
+[Guid("96D33993-317C-4DB5-AFD8-666EE77248E2")]
+public partial interface ICompileRequest : IUnknown
+{
+    //TODO
+}
