333bd3e7a7af5c5ddac6dcd8ac867c8f44c0eadb
AlexisPollonni <alexis.pollonni@gmail.com>
2025-04-08 06:58:06 +0200

    Add experimental IModulePrecompileService interface

---
+++ SlangNet.ComWrappers/Interfaces/IModulePrecompileServiceExperimental.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace SlangNet.ComWrappers;
+
+[GeneratedComInterface(StringMarshalling = StringMarshalling.Utf8)]
+[Guid("8E12E8E3-5FCD-433E-AFCB-13A088BC5EE5")]
+public partial interface IModulePrecompileServiceExperimental
+{
+    [PreserveSig]
+    SlangResult PrecompileForTarget(Unmanaged.CompileTarget target, out IBlob blob);
+    
+    [PreserveSig]
+    SlangResult GetPrecompiledTargetCode(Unmanaged.CompileTarget target, out IBlob code, out IBlob diagnostics);
+
+    [PreserveSig]
+    long GetModuleDependencyCount();
+    
+    [PreserveSig]
+    SlangResult GetModuleDependency(long dependencyIndex, out IModule module, out IBlob diagnostics);
+}
