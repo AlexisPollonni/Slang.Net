db458a43d04b3035bca8d8ae780beb33d0174343
AlexisPollonni <alexis.pollonni@gmail.com>
2025-04-10 19:53:06 +0200

    Completed slang extern Api

---
+++ SlangNet.ComWrappers/Descriptions/GlobalSessionDescription.cs
@@ -0,0 +1,22 @@
+﻿using System.Runtime.InteropServices.Marshalling;
+using SlangNet.Internal;
+
+namespace SlangNet.ComWrappers;
+
+[NativeMarshalling(
+    typeof(MarshalableMarshaller.ManagedToUnmanaged<GlobalSessionDescription, Unmanaged.SlangGlobalSessionDesc>))]
+public readonly unsafe record struct GlobalSessionDescription(
+    uint ApiVersion,
+    Unmanaged.SlangLanguageVersion LanguageVersion = Unmanaged.SlangLanguageVersion.SLANG_LANGUAGE_VERSION_2025,
+    bool EnableGlsl = false) : IMarshalsToNative<Unmanaged.SlangGlobalSessionDesc>
+{
+    Unmanaged.SlangGlobalSessionDesc IMarshalsToNative<Unmanaged.SlangGlobalSessionDesc>.AsNative(
+        ref GrowingStackBuffer buffer) =>
+        new()
+        {
+            structureSize = (uint)sizeof(Unmanaged.SlangGlobalSessionDesc),
+            apiVersion = ApiVersion,
+            languageVersion = (uint)LanguageVersion,
+            enableGLSL = EnableGlsl
+        };
+}
--- SlangNet.ComWrappers/Slang.cs
+++ SlangNet.ComWrappers/Slang.cs
@@ -1,12 +1,25 @@
-﻿using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿using System.Runtime.InteropServices;
 
 namespace SlangNet.ComWrappers;
-
 
 public static partial class Slang
 {
     [LibraryImport("slang", EntryPoint = "slang_createGlobalSession")]
-    public static partial void CreateGlobalSession(long apiVersion, out IGlobalSession globalSession);
-}
+    public static partial SlangResult CreateGlobalSession(nint apiVersion, out IGlobalSession globalSession);
 
+    [LibraryImport("slang", EntryPoint = "slang_createGlobalSession2")]
+    public static partial SlangResult CreateGlobalSession(GlobalSessionDescription desc, out IGlobalSession globalSession);
+
+    [LibraryImport("slang", EntryPoint = "slang_createGlobalSessionWithoutCoreModule")]
+    public static partial SlangResult
+        CreateGlobalSessionWithoutCoreModule(nint apiVersion, out IGlobalSession globalSession);
+
+    [LibraryImport("slang", EntryPoint = "slang_getEmbeddedCoreModule")]
+    public static partial IBlob GetEmbeddedCoreModule();
+
+    [LibraryImport("slang", EntryPoint = "slang_shutdown")]
+    public static partial void Shutdown();
+
+    [LibraryImport("slang", StringMarshalling = StringMarshalling.Utf8, EntryPoint = "slang_getLastInternalErrorMessage")]
+    public static partial string? GetLastInternalErrorMessage();
+}
