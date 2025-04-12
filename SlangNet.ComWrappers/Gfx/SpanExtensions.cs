e121519b77b7d763cdce2e876e794e434f423872
AlexisPollonni <alexis.pollonni@gmail.com>
2025-04-12 14:31:15 +0200

    Add SpanExtensions for memory span conversions
    Introduce a utility class `SpanExtensions` to simplify span creation and casting operations for unmanaged types. This includes methods for creating `Span`, `ReadOnlySpan`, and type-specific spans such as `AsUintSpan` and `AsFloatSpan`.

---
+++ SlangNet.ComWrappers/Gfx/SpanExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.InteropServices;
+using CommunityToolkit.HighPerformance;
+
+namespace SlangNet.ComWrappers;
+
+public static class SpanExtensions
+{
+    public static Span<T> AsSpan<T>(ref this T inline) where T : unmanaged
+    {
+        return MemoryMarshal.CreateSpan(ref inline, 1);
+    }
+
+    public static Span<TCast> AsSpan<T, TCast>(ref this T inline)
+        where T : unmanaged
+        where TCast : unmanaged
+    {
+        return inline.AsSpan().Cast<T, TCast>();
+    }
+
+    public static ReadOnlySpan<T> AsReadOnlySpan<T>(ref this T inline) where T : unmanaged
+    {
+        return MemoryMarshal.CreateReadOnlySpan(in inline, 1);
+    }
+
+    public static ReadOnlySpan<TCast> AsReadOnlySpan<T, TCast>(ref this T inline) 
+        where T : unmanaged 
+        where TCast : unmanaged
+    {
+        return inline.AsReadOnlySpan().Cast<T, TCast>();
+    }
+
+    public static Span<uint> AsUintSpan<T>(ref this T inline) 
+        where T : unmanaged 
+    {
+        return inline.AsSpan<T, uint>();
+    }
+    public static Span<float> AsFloatSpan<T>(ref this T inline) 
+        where T : unmanaged 
+    {
+        return inline.AsSpan<T, float>();
+    }
+}
