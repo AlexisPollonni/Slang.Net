683684d286d7bbaaa88392a8385563d2d8d8d71d
AlexisPollonni <alexis.pollonni@gmail.com>
2025-04-13 14:56:03 +0200

    Implement managed adapters listing and debug callback handling
    Added `GetAdapters` method to retrieve and manage adapter information using a new `AdaptersList` encapsulating native results. Introduced a safe managed implementation of `SetDebugCallback`, preventing GC issues and ensuring native resource cleanup through `DebugCallbackImpl`. Also added `NativeResultReadOnlyList` and `AdapterInfo` structures to streamline adapter handling and marshaling from native types.

---
+++ SlangNet.ComWrappers/Tools/NativeResultReadOnlyList.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SlangNet.ComWrappers.Tools;
+
+// for lists of COM objects returned as pair of
+//   X_GetCount
+//   SlangResult X_getAt(Container*, index, Element**)
+internal abstract class NativeResultReadOnlyList<T> : IReadOnlyList<T>
+{
+    public abstract int Count { get; }
+    public abstract SlangResult TryGetAt(int index, [NotNullWhen(true)] out T? value);
+
+    public T this[int index]
+    {
+        get
+        {
+            TryGetAt(index, out var value).ThrowIfFailed();
+            return value!;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+    public IEnumerator<T> GetEnumerator() => new IndexEnumerator<T>(this);
+}
