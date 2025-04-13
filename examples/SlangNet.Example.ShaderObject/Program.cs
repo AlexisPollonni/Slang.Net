04531573726d4b81bd955a6240c33d084bd55159
Alexis Pollonni <alexis.pollonni@gmail.com>
2025-04-13 14:57:23 +0100

    Fix incorrect span transformations

--- examples/SlangNet.Example.ShaderObject/Program.cs
+++ examples/SlangNet.Example.ShaderObject/Program.cs
@@ -12,121 +12,118 @@ using SlangNet.ComWrappers.Tools.Extensions;
 using SlangNet.Gfx.Tools;
 using Unmanaged = SlangNet.Bindings.Generated;
 
-unsafe
-{
-    var factory = LoggerFactory.Create(builder => builder.AddConsole());
-    var logger = factory.CreateLogger("Gfx");
+var factory = LoggerFactory.Create(builder => builder.AddConsole());
+var logger = factory.CreateLogger("Gfx");
 #if DEBUG
-    Gfx.EnableDebugLayer();
+Gfx.EnableDebugLayer();
 
-    Gfx.SetDebugCallback((type, source, message) =>
-       {
-           var level = type switch
-           {
-               Unmanaged.DebugMessageType.Info => LogLevel.Information,
-               Unmanaged.DebugMessageType.Warning => LogLevel.Warning,
-               Unmanaged.DebugMessageType.Error => LogLevel.Error,
-               _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
-           };
+Gfx.SetDebugCallback((type, source, message) =>
+    {
+        var level = type switch
+        {
+            Unmanaged.DebugMessageType.Info => LogLevel.Information,
+            Unmanaged.DebugMessageType.Warning => LogLevel.Warning,
+            Unmanaged.DebugMessageType.Error => LogLevel.Error,
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
 
-           logger.Log(level, "{Source}: {Message}", source, message);
-       })
-       .ThrowIfFailed();
+        logger.Log(level, "{Source}: {Message}", source, message);
+    })
+    .ThrowIfFailed();
 #endif
 
-    var shaderIncludePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+var shaderIncludePath = Path.GetDirectoryName(typeof(Program).Assembly.Location);
 
-    var adapters = Gfx.GetAdapters(Unmanaged.DeviceType.Vulkan);
-    var devDesc = new DeviceDescription
+var adapters = Gfx.GetAdapters(Unmanaged.DeviceType.Vulkan);
+var devDesc = new DeviceDescription
+{
+    DeviceType = Unmanaged.DeviceType.Vulkan,
+    Slang = new()
     {
-        DeviceType = Unmanaged.DeviceType.Vulkan,
-        Slang = new()
-        {
-            TargetFlags = TargetFlags.GenerateSPIRVDirectly,
-            OptimizationLevel = Unmanaged.OptimizationLevel.None,
-            DefaultMatrixLayoutMode = Unmanaged.MatrixLayoutMode.RowMajor,
-            TargetProfile = "glsl440",
-            SearchPaths = [shaderIncludePath]
-        },
-        ShaderCache = new()
-        {
-            Path = "./ShaderCache"
-        },
-        AdapterLuid = adapters[0].Luid
-    };
-
-    Gfx.CreateDevice(devDesc, out var device).ThrowIfFailed();
-
-    device.CreateTransientResourceHeap(new(ConstantBufferSize: 4096), out var heap).ThrowIfFailed();
-
-    ShaderObjectExample.LoadShaderProgram(device, out var program, out var slangReflection).ThrowIfFailed();
-
-    device.CreateComputePipelineState(new(program), out var pipelineState).ThrowIfFailed();
-
-    const int numberCount = 4;
-    float[] initialData = [0, 1, 2, 3];
-
-    var bufferDesc = new BufferResourceDescription
+        TargetFlags = TargetFlags.GenerateSPIRVDirectly,
+        OptimizationLevel = Unmanaged.OptimizationLevel.None,
+        DefaultMatrixLayoutMode = Unmanaged.MatrixLayoutMode.RowMajor,
+        TargetProfile = "glsl440",
+        SearchPaths = [shaderIncludePath]
+    },
+    ShaderCache = new()
     {
-        Base = new()
-        {
-            AllowedStates = new(Unmanaged.ResourceState.ShaderResource,
-                                Unmanaged.ResourceState.UnorderedAccess,
-                                Unmanaged.ResourceState.CopyDestination,
-                                Unmanaged.ResourceState.CopySource),
-            DefaultState = Unmanaged.ResourceState.UnorderedAccess,
-            MemoryType = Unmanaged.MemoryType.DeviceLocal,
-        },
-        Format = Unmanaged.Format.Unknown,
-        ElementSize = sizeof(float),
-        SizeInBytes = numberCount * sizeof(float)
-    };
+        Path = "./ShaderCache"
+    },
+    AdapterLuid = adapters[0].Luid
+};
+
+Gfx.CreateDevice(devDesc, out var device).ThrowIfFailed();
+
+device.CreateTransientResourceHeap(new(ConstantBufferSize: 4096), out var heap).ThrowIfFailed();
+
+ShaderObjectExample.LoadShaderProgram(device, out var program, out var slangReflection).ThrowIfFailed();
+
+device.CreateComputePipelineState(new(program), out var pipelineState).ThrowIfFailed();
+
+const int numberCount = 4;
+float[] initialData = [0, 1, 2, 3];
+
+var bufferDesc = new BufferResourceDescription
+{
+    Base = new()
+    {
+        AllowedStates = new(Unmanaged.ResourceState.ShaderResource,
+            Unmanaged.ResourceState.UnorderedAccess,
+            Unmanaged.ResourceState.CopyDestination,
+            Unmanaged.ResourceState.CopySource),
+        DefaultState = Unmanaged.ResourceState.UnorderedAccess,
+        MemoryType = Unmanaged.MemoryType.DeviceLocal,
+    },
+    Format = Unmanaged.Format.Unknown,
+    ElementSize = sizeof(float),
+    SizeInBytes = numberCount * sizeof(float)
+};
     
-        device.CreateBufferResource(bufferDesc, in initialData.AsBytes().DangerousGetReference(), out var numbersBuffer).ThrowIfFailed();
+device.CreateBufferResource(bufferDesc, in initialData.AsSpan().AsBytes().DangerousGetReference(), out var numbersBuffer).ThrowIfFailed();
 
-    device.CreateBufferView(numbersBuffer,
-                                             null,
-                                             new()
-                                             {
-                                                 Type = Unmanaged.IResourceView.ResourceViewType.UnorderedAccess,
-                                                 Format = Unmanaged.Format.Unknown
-                                             }, out var bufferView).ThrowIfFailed();
-
-    device.CreateCommandQueue(new(Unmanaged.ICommandQueue.QueueType.Graphics), out var queue).ThrowIfFailed();
-
-    heap.CreateCommandBuffer(out var cmdBuffer).ThrowIfFailed();
-
-    cmdBuffer.EncodeComputeCommands(out var encoder);
-
-    encoder.BindPipeline(pipelineState, out var rootObject).ThrowIfFailed();
-
-    var addTransformerType = slangReflection.FindTypeByName("AddTransformer")!.Value;
-
-    device.CreateShaderObject(addTransformerType, Unmanaged.ShaderObjectContainerType.None, out var transformer).ThrowIfFailed();
-
-    const float c = 1.0f;
-
-    new ShaderCursor(transformer).GetPath("c").SetData(c);
-
-    rootObject.GetEntryPoint(0, out var entryPoint).ThrowIfFailed();
-    var entryPointCursor = new ShaderCursor(entryPoint);
-
-    entryPointCursor.GetPath("buffer").SetResource(bufferView);
-
-    entryPointCursor.GetPath("transformer").SetObject(transformer);
-
-    encoder.DispatchCompute(1, 1, 1);
-    encoder.EndEncoding();
-    cmdBuffer.Close();
-    queue.ExecuteCommandBuffers(1, [cmdBuffer]);
-    queue.WaitOnHost();
-
-    device.ReadBufferResource(numbersBuffer, 0, (nuint)initialData.AsBytes().Length, out var dataBlob).ThrowIfFailed();
-
-    foreach (var item in dataBlob.AsReadOnlySpan().Cast<byte, float>())
+device.CreateBufferView(numbersBuffer,
+    null,
+    new()
     {
-        Console.WriteLine(item);
-    }
+        Type = Unmanaged.IResourceView.ResourceViewType.UnorderedAccess,
+        Format = Unmanaged.Format.Unknown
+    }, out var bufferView).ThrowIfFailed();
+
+device.CreateCommandQueue(new(Unmanaged.ICommandQueue.QueueType.Graphics), out var queue).ThrowIfFailed();
+
+heap.CreateCommandBuffer(out var cmdBuffer).ThrowIfFailed();
+
+cmdBuffer.EncodeComputeCommands(out var encoder);
+
+encoder.BindPipeline(pipelineState, out var rootObject).ThrowIfFailed();
+
+var addTransformerType = slangReflection.FindTypeByName("AddTransformer")!.Value;
+
+device.CreateShaderObject(addTransformerType, Unmanaged.ShaderObjectContainerType.None, out var transformer).ThrowIfFailed();
+
+const float c = 1.0f;
+
+new ShaderCursor(transformer).GetPath("c").SetData(c);
+
+rootObject.GetEntryPoint(0, out var entryPoint).ThrowIfFailed();
+var entryPointCursor = new ShaderCursor(entryPoint);
+
+entryPointCursor.GetPath("buffer").SetResource(bufferView);
+
+entryPointCursor.GetPath("transformer").SetObject(transformer);
+
+encoder.DispatchCompute(1, 1, 1);
+encoder.EndEncoding();
+cmdBuffer.Close();
+queue.ExecuteCommandBuffers(1, [cmdBuffer]);
+queue.WaitOnHost();
+
+device.ReadBufferResource(numbersBuffer, 0, (nuint)initialData.AsSpan().AsBytes().Length, out var dataBlob).ThrowIfFailed();
+
+foreach (var item in dataBlob.AsReadOnlySpan().Cast<byte, float>())
+{
+    Console.WriteLine(item);
 }
 
 static class ShaderObjectExample
