4cb7c561cb02dd35906622fa93652cbcf2312520
AlexisPollonni <alexis.pollonni@gmail.com>
2025-07-29 22:21:14 +0200

    New option enum values

---
+++ SlangNet.Bindings/Generated/FpDenormalMode.cs
@@ -0,0 +1,15 @@
+namespace SlangNet.Bindings.Generated;
+
+/// <include file='FpDenormalMode.xml' path='doc/member[@name="FpDenormalMode"]/*' />
+[NativeTypeName("SlangFpDenormalModeIntegral")]
+public enum FpDenormalMode : uint
+{
+    /// <include file='FpDenormalMode.xml' path='doc/member[@name="FpDenormalMode.FpDenormModeAny"]/*' />
+    FpDenormModeAny = 0,
+
+    /// <include file='FpDenormalMode.xml' path='doc/member[@name="FpDenormalMode.FpDenormModePreserve"]/*' />
+    FpDenormModePreserve,
+
+    /// <include file='FpDenormalMode.xml' path='doc/member[@name="FpDenormalMode.FpDenormModeFtz"]/*' />
+    FpDenormModeFtz,
+}
