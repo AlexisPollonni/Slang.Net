84f9cd62af0040b0e4e15d4b41005b812f563f68
Alexis.Pollonni <43394202+AlexisPollonni@users.noreply.github.com>
2025-10-08 20:59:51 +0200

    Fix: handle null IBlob parameter in BlobExtensions methods

--- SlangNet.ComWrappers/Tools/Extensions/BlobExtensions.cs
+++ SlangNet.ComWrappers/Tools/Extensions/BlobExtensions.cs
@@ -4,16 +4,14 @@ namespace SlangNet.ComWrappers.Tools.Extensions;
 
 public static class BlobExtensions
 {
-    extension(IBlob blob)
+    extension(IBlob? blob)
     {
-        public unsafe ReadOnlySpan<byte> AsReadOnlySpan() =>
-            new(blob.GetBufferPointer(), checked((int)blob.GetBufferSize()));
-
         public unsafe ReadOnlySpan<T> AsReadOnlySpan<T>() where T : unmanaged =>
-            new(blob.GetBufferPointer(), checked((int)(blob.GetBufferSize() / (ulong)sizeof(T))));
+            blob is null ? ReadOnlySpan<T>.Empty : new(blob.GetBufferPointer(), checked((int)(blob.GetBufferSize() / (ulong)sizeof(T))));
 
         public unsafe string? AsString()
         {
+            if (blob is null) return null;
             var ptr = blob.GetBufferPointer();
             if (ptr is null) return null;
         
