7927c256f168a649fa5bbe4bb2c28ab4adf5fc14
Alexis.Pollonni <43394202+alexispollonni@users.noreply.github.com>
2026-02-27 13:53:02 +0100

    feat: add pass to generate static classes for functions with matching prefixes

---
+++ Scripts/ShaderSlang.Net.Scripts.CppSharpGenerator/GenerateStaticClassForFunction.cs
@@ -0,0 +1,56 @@
+using CppSharp;
+using CppSharp.AST;
+using CppSharp.Passes;
+
+namespace ShaderSlang.Net.Scripts.CppSharpGenerator;
+
+internal sealed class GenerateStaticClassForFunction(string functionPrefix) : TranslationUnitPass
+{
+    public override bool VisitMethodDecl(Method method) =>
+        true;
+
+    public override bool VisitFunctionDecl(Function function)
+    {
+        if (!function.IsGenerated) return false;
+
+        if (!function.OriginalName.StartsWith(functionPrefix, StringComparison.OrdinalIgnoreCase))
+        {
+            return false;
+        }
+
+        var newName = function.OriginalName.Remove(0, functionPrefix.Length);
+
+        newName = StringHelpers.Capitalize(newName);
+
+        var className = functionPrefix.Trim('_');
+
+        newName = newName.Insert(0, className);
+
+        function.Name = newName;
+
+        GetOrCreateClass(function, className);
+
+        return true;
+    }
+
+    private static Class GetOrCreateClass(Function function, string className)
+    {
+        var nGlobal = function.Namespace;
+
+        var staticClass = nGlobal.FindClass(className);
+
+        if (staticClass is not null) return staticClass;
+
+        staticClass = new()
+        {
+            Name = className,
+            IsStatic = true,
+            Namespace = nGlobal,
+            Access = AccessSpecifier.Public,
+        };
+
+        nGlobal.Declarations.Add(staticClass);
+        
+        return staticClass;
+    }
+}
